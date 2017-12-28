363a3650539e841c35bb261c3b978f099a6d60d3
hxy0229 <e0134134@u.nus.edu>
2017-12-28 16:22:01 +0800

    Fininsh the general structure:

--- src/functionalCode/Main.java
+++ src/functionalCode/Main.java
@@ -1,21 +1,70 @@
 package functionalCode;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/fxml/Sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Chinese Chess!");
+
+        initRootLayout();
+
+        showMainWindow();
     }
 
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/fxml/RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showMainWindow() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/fxml/MainWindow.fxml"));
+            AnchorPane mainWindow= loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(mainWindow);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/functionalCode/view/ui/MainWindow.java
+++ src/functionalCode/view/ui/MainWindow.java
@@ -1,4 +1,13 @@
 package functionalCode.view.ui;
 
+import java.util.HashMap;
+import java.util.Random;
+
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class MainWindow {
 }
